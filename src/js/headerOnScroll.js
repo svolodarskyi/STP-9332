b79113fa339efd0b040190a8c6c9f8b83c8a970b
svolodarskyi <sergii.volodarskyi@gmail.com>
2025-07-24 16:58:05 -0600

    refactor class selectors into data attributes

--- src/js/headerOnScroll.js
+++ src/js/headerOnScroll.js
@@ -1,28 +1,3 @@
-// const header = document.getElementById('header');
-
-// function updateHeaderOnScroll() {
-//   if (window.innerWidth >= 1200) {
-//     if (window.scrollY > 10) {
-//       header.classList.add('scrolled');
-//       document.querySelectorAll('.nav-item').forEach(el => {
-//         el.style.color = 'white';
-//       });
-//       header.classList.remove('at-top');
-//     } else {
-//       header.classList.remove('scrolled');
-//       document.querySelectorAll('.nav-item').forEach(el => {
-//         el.style.color = 'black';
-//       });
-//       header.classList.add('at-top');
-//     }
-//   } else {
-//     header.classList.remove('scrolled');
-//     header.classList.add('at-top');
-//   }
-// }
-
-// window.addEventListener('scroll', updateHeaderOnScroll);
-
 const header = document.querySelector('[data-header]');
 
 function updateHeaderOnScroll() {
